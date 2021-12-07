abe04a308670baec85a7c39d7293c6d63c768f59
Rakshith658 <rakshithkumars7777@gmail.com>
2021-12-07 19:12:58 +0530

    GB part is done

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import { GraphQLServer } from "graphql-yoga";
 
 const users = [
   {
-    id: "123e",
+    id: "1",
     name: "John",
     email: "John@example.com",
     age: null,
   },
   {
-    id: "1",
+    id: "2",
     name: "Raju",
     email: "Raju@example.com",
     age: 24,
   },
   {
-    id: "12",
+    id: "3",
     name: "rakshith",
     email: "rakshith@example.com",
     age: 19,
@@ -27,18 +27,48 @@ const posts = [
     title: "programming",
     body: "i am pragramer",
     published: true,
+    author: "1",
   },
   {
     id: "2",
     title: "Stock Market",
     body: "Rakshith is investor",
     published: true,
+    author: "1",
   },
   {
     id: "3",
     title: "Lover",
     body: "Rakshith loves Chaitra",
     published: true,
+    author: "2",
+  },
+];
+
+const Comments = [
+  {
+    id: "1",
+    postid: "1",
+    authorid: "1",
+    body: "super post",
+  },
+  {
+    id: "2",
+    postid: "1",
+    authorid: "1",
+    body: "super Rakshith",
+  },
+  {
+    id: "3",
+    postid: "2",
+    authorid: "1",
+    body: "fine loves",
+  },
+  {
+    id: "4",
+    postid: "3",
+    authorid: "2",
+    body: "i likes this",
   },
 ];
 
@@ -46,6 +76,7 @@ const typeDefs = `
     type Query {
       Users(query:String):[User]
       Posts(query:String):[Post]
+      Comments(query:String):[comment]
         me:User!
         post:Post!
     }
@@ -54,24 +85,31 @@ const typeDefs = `
         name:String!
         email:String!
         age:Int
+        posts:[Post]!
+        comments:[comment]!
     }
     type Post {
         id:ID!
         title:String!
         body:String!
         published:Boolean
+        author:User!
+        comments:[comment]!
+    }
+    type comment{
+      id:ID!
+      postid:String!
+      authorid:String!
+      post:Post!
+      author:User!
+      body:String!
     }
     `;
 
 const resolvers = {
   Query: {
     me() {
-      return {
-        id: "123e",
-        name: "John",
-        email: "John@example.com",
-        age: null,
-      };
+      return users[0];
     },
     Users(parent, args, ctx, info) {
       if (!args.query) {
@@ -79,6 +117,16 @@ const resolvers = {
       }
       return users.filter((User) =>
         User.name.toLocaleLowerCase().includes(args.query.toLocaleLowerCase())
+      );
+    },
+    Comments(parent, args, ctx, info) {
+      if (!args.query) {
+        return Comments;
+      }
+      return Comments.filter((Comment) =>
+        Comment.body
+          .toLocaleLowerCase()
+          .includes(args.query.toLocaleLowerCase())
       );
     },
     Posts(parent, args, ctx, info) {
@@ -96,12 +144,31 @@ const resolvers = {
       });
     },
     post() {
-      return {
-        id: "1",
-        title: "John",
-        body: "Rakshith Kumar S is a king of his owm kingdom",
-        published: false,
-      };
+      return posts[0];
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    comments(parent, args, ctx, info) {
+      return Comments.filter((comment) => comment.postid === parent.id);
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => post.author === parent.id);
+    },
+    comments(parent, args, ctx, info) {
+      return Comments.filter((comment) => comment.authorid === parent.id);
+    },
+  },
+  comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.authorid);
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => post.id === parent.postid);
     },
   },
 };
